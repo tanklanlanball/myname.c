ad5d264c8ede905523db042d8a19371eb3a44a7f
tanklanlanball <90494929+tanklanlanball@users.noreply.github.com>
2022-04-15 15:41:43 +0800

    Update triangle.c

--- triangle.c
+++ triangle.c
@@ -5,9 +5,9 @@ main () {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n-i; j++) {
         if( i % 2 ) {
-           printf("*");
+           printf("+");
            } else {
-              printf("+");
+              printf("*");
               }
         }
         printf("\n");
